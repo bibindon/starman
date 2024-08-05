3c5100aa88587dce21b4b424e26566b7721ea555
bibindon0814 <bibindon0814@gmail.com>
2024-08-05 10:18:56 +0900

    .

--- starman/starman/main.cpp
+++ starman/starman/main.cpp
@@ -13,8 +13,13 @@ _TCHAR gName[100] = _T("3Dオブジェクト描画サンプルプログラム");
 
 
 // ウィンドウプロシージャ
-LRESULT CALLBACK WndProc(HWND hWnd, UINT mes, WPARAM wParam, LPARAM lParam) {
-    if (mes == WM_DESTROY) { PostQuitMessage(0); return 0; }
+LRESULT CALLBACK WndProc(HWND hWnd, UINT mes, WPARAM wParam, LPARAM lParam)
+{
+    if (mes == WM_DESTROY)
+    {
+        PostQuitMessage(0);
+        return 0;
+    }
     return DefWindowProc(hWnd, mes, wParam, lParam);
 }
 
@@ -22,30 +27,66 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT mes, WPARAM wParam, LPARAM lParam) {
 int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
 {
     // アプリケーションの初期化
-    MSG msg; HWND hWnd;
-    WNDCLASSEX wcex = { sizeof(WNDCLASSEX), CS_HREDRAW | CS_VREDRAW, WndProc, 0, 0, hInstance, NULL, NULL,
-                                     (HBRUSH)(COLOR_WINDOW + 1), NULL, (_TCHAR*)gName, NULL };
+    MSG msg;
+    HWND hWnd;
+    WNDCLASSEX wcex = {
+        sizeof(WNDCLASSEX),
+        CS_HREDRAW | CS_VREDRAW,
+        WndProc,
+        0,
+        0,
+        hInstance,
+        NULL,
+        NULL,
+        (HBRUSH)(COLOR_WINDOW + 1),
+        NULL,
+        (_TCHAR*)gName, NULL
+    };
+
     if (!RegisterClassEx(&wcex))
+    {
         return 0;
+    }
 
     if (!(hWnd = CreateWindow(gName, gName, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0,
         NULL, NULL, hInstance, NULL)))
+    {
         return 0;
+    }
 
     // Direct3Dの初期化
     LPDIRECT3D9 g_pD3D;
     LPDIRECT3DDEVICE9 g_pD3DDev;
-    if (!(g_pD3D = Direct3DCreate9(D3D_SDK_VERSION))) return 0;
+    if (!(g_pD3D = Direct3DCreate9(D3D_SDK_VERSION)))
+    {
+        return 0;
+    }
 
-    D3DPRESENT_PARAMETERS d3dpp = { 0,0,D3DFMT_UNKNOWN,0,D3DMULTISAMPLE_NONE,0,
-                                                       D3DSWAPEFFECT_DISCARD,NULL,TRUE,TRUE,D3DFMT_D24S8,0,D3DPRESENT_RATE_DEFAULT,D3DPRESENT_INTERVAL_DEFAULT };
+    D3DPRESENT_PARAMETERS d3dpp = {
+        0,
+        0,
+        D3DFMT_UNKNOWN,
+        0,
+        D3DMULTISAMPLE_NONE,
+        0,
+        D3DSWAPEFFECT_DISCARD,
+        NULL,
+        TRUE,
+        TRUE,
+        D3DFMT_D24S8,
+        0,
+        D3DPRESENT_RATE_DEFAULT,
+        D3DPRESENT_INTERVAL_DEFAULT
+    };
 
     if (FAILED(g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &d3dpp, &g_pD3DDev)))
+    {
         if (FAILED(g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &g_pD3DDev)))
         {
             g_pD3D->Release();
             return 0;
         }
+    }
 
     // 立方体オブジェクト生成
     ID3DXBuffer* pMaterials;
@@ -84,10 +125,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCm
     // ウィンドウ表示
     ShowWindow(hWnd, SW_SHOW);
 
-
-    ////////////////////////////
     // メッセージループ
-    /////
     D3DXMATRIX World;          // 立方体ワールド変換行列
     D3DXMATRIX Rot_X, Rot_Y;   // 立方体回転行列
     D3DXMATRIX Offset;         // 立方体オフセット行列
@@ -101,7 +139,10 @@ int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCm
     unsigned int i;
     do {
         Sleep(1);
-        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) { DispatchMessage(&msg); }
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            DispatchMessage(&msg);
+        }
         g_pD3DDev->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(40, 40, 80), 1.0f, 0);
         g_pD3DDev->BeginScene();
 
@@ -111,8 +152,8 @@ int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCm
         // ライト
         ////////
             // ライトの方向回転
-        light.Direction.x = 20 * sin(D3DXToRadian(Ang));
-        light.Direction.z = 20 * cos(D3DXToRadian(Ang));
+        light.Direction.x = 20 * sinf(D3DXToRadian(Ang));
+        light.Direction.z = 20 * cosf(D3DXToRadian(Ang));
         g_pD3DDev->SetLight(0, &light);
 
         ///////////////////////////
@@ -129,9 +170,9 @@ int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCm
 
     // ビュー変換
         // 視点は原点固定ですが、カメラの位置は適当です
-        D3DXVECTOR3 a = D3DXVECTOR3(47.5f * cos(D3DXToRadian(Ang * 0.7f)), 70.2f * cos(D3DXToRadian(Ang * 0.2f)), 56.7f * sin(D3DXToRadian(Ang * 1.2f)));
+        D3DXVECTOR3 a = D3DXVECTOR3(47.5f * cosf(D3DXToRadian(Ang * 0.7f)), 70.2f * cosf(D3DXToRadian(Ang * 0.2f)), 56.7f * sinf(D3DXToRadian(Ang * 1.2f)));
         D3DXVECTOR3 b = D3DXVECTOR3(0, 0, 0);
-        D3DXVECTOR3 c = D3DXVECTOR3(sin(D3DXToRadian(Ang * 0.4f)), 1, 0);
+        D3DXVECTOR3 c = D3DXVECTOR3(sinf(D3DXToRadian(Ang * 0.4f)), 1, 0);
         D3DXMatrixLookAtLH(
             &View,
             &a,
@@ -161,7 +202,9 @@ int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCm
         // ワールド変換（単位行列のみ）
         g_pD3DDev->SetTransform(D3DTS_WORLD, &AXWorld);
 
-        for (i = 0; i < AX_NumMaterials; i++) {      // 軸描画
+        for (i = 0; i < AX_NumMaterials; i++)
+        {
+            // 軸描画
             D3DXMATERIAL* mtrl = ((D3DXMATERIAL*)(pAX_Materials->GetBufferPointer()) + i);
             mtrl->MatD3D.Ambient = MAmbient;
             g_pD3DDev->SetMaterial(&mtrl->MatD3D);
