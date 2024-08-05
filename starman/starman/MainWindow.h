5c024b5fa858e39eac0c0a59915fd48196caf8fa
bibindon0814 <bibindon0814@gmail.com>
2024-08-05 15:47:18 +0900

    .

--- starman/starman/MainWindow.h
+++ starman/starman/MainWindow.h
@@ -1,5 +1,28 @@
 #pragma once
+
+#include <windows.h>
+#include <tchar.h>
+#include <d3d9.h>
+#include <d3dx9.h>
+
 class MainWindow
 {
+public:
+    explicit MainWindow(const HINSTANCE&);
+    ~MainWindow();
+    int MainLoop();
+
+private:
+    MSG msg;
+    HWND hWnd;
+    LPDIRECT3D9 g_pD3D;
+    LPDIRECT3DDEVICE9 g_pD3DDev;
+    D3DLIGHT9 light;
+    ID3DXBuffer* pMaterials;
+    DWORD NumMaterials;
+    ID3DXMesh* pMesh;
+    ID3DXBuffer* pAX_Materials;
+    DWORD AX_NumMaterials;
+    ID3DXMesh* pAX_Mesh;
 };
 
