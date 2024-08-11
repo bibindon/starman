cfbab9481a0a4eb2be92294986ac6d37449e0e87
bibindon <bibindon0814@gmail.com>
2024-08-11 15:57:33 +0900

    .

--- starman/starman/SeqTitle.cpp
+++ starman/starman/SeqTitle.cpp
@@ -11,8 +11,13 @@ SeqTitle::SeqTitle()
 {
     m_sprite1 = new Sprite("res\\image\\title01.png");
     m_sprite2 = new Sprite("res\\image\\title02.png");
+
     m_sprite3 = new Sprite("res\\image\\title03.png");
     m_sprite3->SetFill(true);
+
+    m_spriteBlack = new Sprite("res\\image\\black.png");
+    m_spriteBlack->SetFill(true);
+
     m_spriteCursor = new Sprite("res\\image\\cursor.png");
 
     SoundEffect::get_ton()->load("res\\sound\\cursor_move.wav");
@@ -26,6 +31,7 @@ SeqTitle::~SeqTitle()
     SAFE_DELETE(m_sprite1);
     SAFE_DELETE(m_sprite2);
     SAFE_DELETE(m_sprite3);
+    SAFE_DELETE(m_spriteBlack);
     SAFE_DELETE(m_spriteCursor);
 }
 
@@ -79,24 +85,7 @@ void SeqTitle::Update(eSequence* sequence)
     }
     else if (KeyBoard::IsDown(DIK_RETURN))
     {
-        switch (m_eMenu)
-        {
-        case eMenu::START:
-        {
-            *sequence = eSequence::OPENING;
-            break;
-        }
-        case eMenu::CONTINUE:
-        {
-            *sequence = eSequence::BATTLE;
-            break;
-        }
-        case eMenu::EXIT:
-        {
-            *sequence = eSequence::EXIT;
-            break;
-        }
-        }
+        m_bFadeOut = true;
         SoundEffect::get_ton()->play("res\\sound\\cursor_confirm.wav", 90);
     }
     if (JoyStick::IsDown(eJoyStickButtonType::UP))
@@ -109,25 +98,44 @@ void SeqTitle::Update(eSequence* sequence)
     }
     else if (JoyStick::IsDown(eJoyStickButtonType::A))
     {
-        switch (m_eMenu)
-        {
-        case eMenu::START:
-        {
-            *sequence = eSequence::OPENING;
-            break;
-        }
-        case eMenu::CONTINUE:
-        {
-            *sequence = eSequence::BATTLE;
-            break;
-        }
-        case eMenu::EXIT:
-        {
-            *sequence = eSequence::EXIT;
-            break;
-        }
-        }
+        m_bFadeOut = true;
         SoundEffect::get_ton()->play("res\\sound\\cursor_confirm.wav", 90);
+    }
+    if (m_bFadeIn)
+    {
+        ++m_fadeInCount;
+        if (m_fadeInCount >= FADE_IN_COUNT)
+        {
+            m_fadeInCount = FADE_IN_COUNT;
+            m_bFadeIn = false;
+        }
+    }
+    if (m_bFadeOut)
+    {
+        ++m_fadeOutCount;
+        if (m_fadeOutCount >= FADE_OUT_COUNT)
+        {
+            m_fadeOutCount = FADE_OUT_COUNT;
+            m_bFadeOut = false;
+            switch (m_eMenu)
+            {
+            case eMenu::START:
+            {
+                *sequence = eSequence::OPENING;
+                break;
+            }
+            case eMenu::CONTINUE:
+            {
+                *sequence = eSequence::BATTLE;
+                break;
+            }
+            case eMenu::EXIT:
+            {
+                *sequence = eSequence::EXIT;
+                break;
+            }
+            }
+        }
     }
 }
 
@@ -162,4 +170,15 @@ void SeqTitle::Render()
     }
     }
     m_spriteCursor->Render(pos3);
+
+    if (m_bFadeIn)
+    {
+        D3DXVECTOR3 pos { 0.0f, 0.0f, 0.0f };
+        m_spriteBlack->Render(pos, 255 - (int)(255.f * m_fadeInCount / FADE_IN_COUNT));
+    }
+    if (m_bFadeOut)
+    {
+        D3DXVECTOR3 pos { 0.0f, 0.0f, 0.0f };
+        m_spriteBlack->Render(pos, (int)(255.f * m_fadeOutCount / FADE_OUT_COUNT));
+    }
 }
--- starman/starman/SeqTitle.h
+++ starman/starman/SeqTitle.h
@@ -14,6 +14,7 @@ private:
     Sprite* m_sprite1 { nullptr };
     Sprite* m_sprite2 { nullptr };
     Sprite* m_sprite3 { nullptr };
+    Sprite* m_spriteBlack { nullptr };
     Sprite* m_spriteCursor { nullptr };
     enum class eMenu
     {
@@ -24,5 +25,9 @@ private:
     eMenu m_eMenu { eMenu::START };
     bool m_bFadeIn { true };
     bool m_bFadeOut { false };
+    int m_fadeInCount { 0 };
+    int m_fadeOutCount { 0 };
+    const int FADE_IN_COUNT { 60 };
+    const int FADE_OUT_COUNT { 60 };
 };
 
