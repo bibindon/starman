5a0d46f94fd9c18659b336459987e303926cc70b
bibindon <bibindon0814@gmail.com>
2024-08-24 22:48:55 +0900

    .

---
+++ starman/Launcher/Launcher.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+#include <WinBase.h>
+#include <WinUser.h>
+
+int main()
+{
+    STARTUPINFO si {};
+    PROCESS_INFORMATION pi {};
+    si.cb = sizeof(si);
+
+    CreateProcess(".\\starman\\game.exe", nullptr, nullptr, nullptr, false, 0, nullptr, ".\\starman", &si, &pi);
+
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    return EXIT_SUCCESS;
+}
+
