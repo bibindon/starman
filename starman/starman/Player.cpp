755d6672b9e31a0d8c00825b807b0d8de7ddd0d3
bibindon <bibindon0814@gmail.com>
2025-04-08 22:32:20 +0900

    .

--- starman/starman/Player.cpp
+++ starman/starman/Player.cpp
@@ -639,12 +639,12 @@ void Player::Update(Map* map)
         if (m_bUnderwater == false)
         {
 //            MAX_XZ_MOVE = 5.0f;
-            MAX_XZ_MOVE = 0.5f;
+            MAX_XZ_MOVE = 1.5f;
 //            MAX_XZ_MOVE = 5000.0f;
         }
         else
         {
-            MAX_XZ_MOVE = 0.1f;
+            MAX_XZ_MOVE = 1.1f;
  //           MAX_XZ_MOVE = 10000.0f;
         }
     }
