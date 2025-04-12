8c41749ffdc9b6d0b1382277cbb75ee33eec67a4
bibindon <bibindon0814@gmail.com>
2025-04-13 07:55:42 +0900

    .

--- starman/starman/SeqBattle.cpp
+++ starman/starman/SeqBattle.cpp
@@ -1744,6 +1744,16 @@ void SeqBattle::OperateQuest(eSequence* sequence)
                         }
                     }
                 }
+                else if (vs2.at(j).find("<move>") != std::string::npos)
+                {
+                    std::string work = vs2.at(j);
+                    std::string work2 = Common::RemoveSubstring(work, "<showMenu>");
+                    std::vector<std::string> vs = Common::split(work2, ':');
+                    float fx = std::stof(vs.at(0));
+                    float fy = std::stof(vs.at(1));
+                    float fz = std::stof(vs.at(2));
+                    m_player->SetPos(D3DXVECTOR3(fx, fy, fz));
+                }
             }
         }
     }
