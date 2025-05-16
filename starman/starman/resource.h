ec1b47007790e4f5f187708bb7c849be6d025e23
bibindon <bibindon0814@gmail.com>
2025-05-16 13:01:41 +0900

    s

--- starman/starman/resource.h
+++ starman/starman/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ ‚Å¶¬‚³‚ê‚½ƒCƒ“ƒNƒ‹[ƒh ƒtƒ@ƒCƒ‹B
-// starman.rc ‚Åg—p
+// Microsoft Visual C++ ã§ç”Ÿæˆã•ã‚ŒãŸã‚¤ãƒ³ã‚¯ãƒ«ãƒ¼ãƒ‰ ãƒ•ã‚¡ã‚¤ãƒ«ã€‚
+// starman.rc ã§ä½¿ç”¨
 //
 #define IDI_ICON1                       101
 #define IDS_STRING101                   101
