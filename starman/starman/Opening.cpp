a6684717e683020d073d81ec4355396754ed54d7
bibindon <bibindon0814@gmail.com>
2025-06-13 21:10:10 +0900

    s

--- starman/starman/Opening.cpp
+++ starman/starman/Opening.cpp
@@ -182,7 +182,14 @@ Opening::Opening()
         {
             Page page;
             ISprite* sprite = NEW NSStoryTelling::Sprite(SharedObj::GetD3DDevice());
-            sprite->Load(_T("res\\image\\opening01.png"));
+            if (!SharedObj::IsEnglish())
+            {
+				sprite->Load(_T("res\\image\\opening01.png"));
+            }
+            else
+            {
+				sprite->Load(_T("res\\image\\opening01.en.png"));
+            }
             page.SetSprite(sprite);
             std::vector<std::vector<std::wstring> > vvs;
             std::vector<std::wstring> vs;
@@ -198,9 +205,20 @@ Opening::Opening()
             page.SetSprite(sprite);
             std::vector<std::vector<std::wstring> > vvs;
             std::vector<std::wstring> vs;
-            vs.push_back(_T("あるところにパワーエッグ星という惑星があった。"));
-            vs.push_back(_T("パワーエッグ星には海といくつかの大陸、そして無数の島があった。"));
-            vs.push_back(_T("その島の中にプロリタン島という島があった。"));
+
+            if (!SharedObj::IsEnglish())
+            {
+                vs.push_back(_T("あるところにパワーエッグ星という惑星があった。"));
+                vs.push_back(_T("パワーエッグ星には海といくつかの大陸、そして無数の島があった。"));
+                vs.push_back(_T("その島の中にプロリタン島という島があった。"));
+            }
+            else
+            {
+                vs.push_back(_T("Once upon a time, there was a planet called Poweregg Planet."));
+                vs.push_back(_T("Poweregg Planet had oceans, several continents, and countless islands."));
+                vs.push_back(_T("Among those islands was one called Prolitan Island."));
+            }
+
             vvs.push_back(vs);
             page.SetTextList(vvs);
             pageList.push_back(page);
@@ -212,13 +230,27 @@ Opening::Opening()
             page.SetSprite(sprite);
             std::vector<std::vector<std::wstring> > vvs;
             std::vector<std::wstring> vs;
-            vs.push_back(_T("プロリタン島の、入り江の木の下で一人の男性が寝ていた。寝ていた男性、ホシマンは浜辺で目を覚ました。"));
-            vs.push_back(_T("ホシマンは頭部が星の形をしていた。星の形をしているうえに全身黄色でまさに星という感じであった。"));
-            vvs.push_back(vs);
-            vs.clear();
-            vs.push_back(_T("「し、死ぬ・・・」"));
-            vs.push_back(_T("かすれ声だった。"));
-            vvs.push_back(vs);
+
+            if (!SharedObj::IsEnglish())
+            {
+                vs.push_back(_T("プロリタン島の、入り江の木の下で一人の男性が寝ていた。寝ていた男性、ホシマンは浜辺で目を覚ました。"));
+                vs.push_back(_T("ホシマンは頭部が星の形をしていた。星の形をしているうえに全身黄色でまさに星という感じであった。"));
+                vvs.push_back(vs);
+                vs.clear();
+                vs.push_back(_T("「し、死ぬ・・・」"));
+                vs.push_back(_T("かすれ声だった。"));
+                vvs.push_back(vs);
+            }
+            else
+            {
+                vs.push_back(_T("Under a tree by the inlet on Prolitan Island, a man lay sleeping. That man, Star Man, woke up on the beach."));
+                vs.push_back(_T("His head was shaped like a star. With his entire body yellow as well, he truly looked like a star."));
+                vvs.push_back(vs);
+                vs.clear();
+                vs.push_back(_T("\"I... I'm dying...\""));
+                vs.push_back(_T("His voice was barely a whisper."));
+                vvs.push_back(vs);
+            }
             page.SetTextList(vvs);
             pageList.push_back(page);
         }
@@ -229,16 +261,38 @@ Opening::Opening()
             page.SetSprite(sprite);
             std::vector<std::vector<std::wstring> > vvs;
             std::vector<std::wstring> vs;
-            vs.clear();
-            vs.push_back(_T("ホシマンはバイクでツーリングを楽しむため船で東のオプティプル島に移動中だった。"));
-            vs.push_back(_T("しかし、格安ツアーだったため台風でも強行された。"));
-            vs.push_back(_T("そして、元からボロボロだった船は亀裂から浸水が起こり、船が傾きやがて転覆してしまった。"));
-            vvs.push_back(vs);
-            vs.clear();
-            vs.push_back(_T("船が転覆したとき、船体は砕け、ホシマンは船の瓦礫に掴まった。そのあと寝ずに３０時間過ごした。"));
-            vs.push_back(_T("漂流して３０時間後、島の姿が近くに現れた。"));
-            vs.push_back(_T("ホシマンは無我夢中になり、瓦礫をオールにして島まで漕ぎ、島にたどり着くと浜辺に倒れこんだ。そして、そのまま意識を失った。"));
-            vvs.push_back(vs);
+
+            if (!SharedObj::IsEnglish())
+            {
+				vs.clear();
+				vs.push_back(_T("ホシマンはバイクでツーリングを楽しむため船で東のオプティプル島に移動中だった。"));
+				vs.push_back(_T("しかし、格安ツアーだったため台風でも強行された。"));
+				vs.push_back(_T("そして、元からボロボロだった船は亀裂から浸水が起こり、船が傾きやがて転覆してしまった。"));
+				vvs.push_back(vs);
+				vs.clear();
+				vs.push_back(_T("船が転覆したとき、船体は砕け、ホシマンは船の瓦礫に掴まった。そのあと寝ずに３０時間過ごした。"));
+				vs.push_back(_T("漂流して３０時間後、島の姿が近くに現れた。"));
+				vs.push_back(_T("ホシマンは無我夢中になり、瓦礫をオールにして島まで漕ぎ、島にたどり着くと浜辺に倒れこんだ。そして、そのまま意識を失った。"));
+				vvs.push_back(vs);
+			}
+            else
+            {
+                vs.clear();
+                vs.push_back(_T("Star Man was on a ferry heading east to Optiple Island for a motorcycle tour."));
+                vs.push_back(_T("However, because it was a budget tour, the trip went ahead despite a typhoon."));
+                vs.push_back(_T("The ship, already in poor condition, began taking on water through cracks, tilted, and eventually capsized."));
+                vvs.push_back(vs);
+                vs.clear();
+                vs.push_back(_T("When the ship capsized, the hull broke apart, and Star Man clung to the debris."));
+                vs.push_back(_T("He then spent 30 hours without sleep."));
+                vs.push_back(_T("After drifting for 30 hours, an island came into view nearby."));
+                vvs.push_back(vs);
+                vs.clear();
+                vs.push_back(_T("Driven by sheer desperation, Star Man used the wreckage as a paddle and rowed to the island."));
+                vs.push_back(_T("Upon reaching shore, he collapsed and lost consciousness."));
+                vvs.push_back(vs);
+            }
+
             page.SetTextList(vvs);
             pageList.push_back(page);
         }
